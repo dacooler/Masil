c2770e31176ce00cf9927d1b6c9b2552f66d585f
dacoolest <oscar.jansson1@proton.me>
2026-02-20 19:41:30 +0000

    hej2

---
+++ nav/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+  <head>
+  <link rel="stylesheet" href="src/camera.css">
+  <link rel="stylesheet" href="src/movement.css">
+  <link rel="stylesheet" href="src/index.css">
+  </head>
+  <body>
+    <div class="main">
+      <?php
+        require("src/camera.php");
+      ?>
+    </div>
+  </body>
+</html>
