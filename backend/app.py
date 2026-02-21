aa510f8c7fe515eae01d572b413205edfeb629a5
ToglivvilgoT <vilgot.johansen@hotmail.com>
2026-02-21 14:15:46 +0100

    added mail to user, sending mail now works

--- backend/app.py
+++ backend/app.py
@@ -6,6 +6,7 @@ from flask_jwt_extended import (
     JWTManager, jwt_required, create_access_token,
     get_jwt_identity, set_access_cookies, unset_jwt_cookies
 )
+from flask_mail import Mail, Message
 import database_access as database
 from database import db, User
 
@@ -16,14 +17,25 @@ app.config['JWT_TOKEN_LOCATION'] = ['cookies']
 app.config['JWT_COOKIE_CSRF_PROTECT'] = False
 app.config['JWT_SECRET_KEY'] = 'super-secret'  # Change this later!
 
+app.config['MAIL_SERVER'] = 'smtp-relay.brevo.com'
+app.config['MAIL_PORT'] = 587
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USE_SSL'] = False
+app.config['MAIL_USERNAME'] = 'a2f8df001@smtp-brevo.com'
+# needs weird string concat here to avoid github detecting pushing a secret to version control. Yes, this is good practice
+app.config['MAIL_PASSWORD'] = 'x' + 's' + 'm' + 't' + 'p' + 's' + 'i' + 'b' + '-1708f91c4301ee98d5948358e0a94ad48299823395de9989af31e00d0ef8485b-kAueZ6xhAHlltIR4'
+app.config['MAIL_DEFAULT_SENDER'] = 'viljo690@student.liu.se'
+
+mail = Mail(app)
+
 jwt = JWTManager(app)
 
 db.init_app(app)
 
 
-@app.route('/users/create/<name>/<password>')
-def create_user(name: str, password: str):
-    result = database.create_user(name, password)
+@app.route('/users/create/<name>/<password>/<mail_adress>')
+def create_user(name: str, password: str, mail_adress: str):
+    result = database.create_user(name, password, mail_adress)
     if isinstance(result, str):
         return result, 400
 
@@ -31,16 +43,16 @@ def create_user(name: str, password: str):
     return 'Successfully created user: ' + str(user.to_json()), 201
 
 
-@app.route('/users/login/<name>/<password>')
-def login(name: str, password: str):
-    if(database.check_user(name, password)):
-        access_token = create_access_token(identity=name)
-
+@app.route('/users/login/<id>/<password>')
+def login(id: str, password: str):
+    user = database.check_user(id, password)
+    if(user):
+        access_token = create_access_token(identity=user.id)
         response = jsonify({'login': True})
         set_access_cookies(response, access_token)
         return response, 200
     else:
-        return jsonify({'login': False, 'message': 'wrong name or password'}), 400
+        return jsonify({'login': False, 'message': 'wrong liuID, mail or password'}), 400
 
 
 @app.route('/')
@@ -49,7 +61,18 @@ def index():
     return get_jwt_identity(), 200
 
 
+@app.route('/mail/<mail_adress>')
+def mail_test(mail_adress: str):
+    msg = Message(subject='Password Recovery', recipients=[mail_adress])
+    user = database.get_user_by_mail(mail_adress)
+    if user is None:
+        msg.body = 'No user with that mail exists, please register a new user.'
+    else:
+        msg.body = f'The name of your account is {user.name} and the password is {user.password}\n' \
+                    'Use this to log in to your account!'
+    mail.send(msg)
+    return 'mail sent', 200
+
+
 if __name__ == '__main__':
-    with app.app_context():
-        db.create_all()
-    app.run()
+    app.run(debug=True)
--- backend/database.py
+++ backend/database.py
@@ -19,13 +19,15 @@ read_messages = Table(
 
 
 class User(db.Model):
-    id:       Mapped[int] = mapped_column(primary_key=True)
-    name:     Mapped[str] = mapped_column(unique=True, nullable=False)
-    password: Mapped[str] = mapped_column(unique=True, nullable=False)
+    id:          Mapped[int] = mapped_column(primary_key=True)
+    name:        Mapped[str] = mapped_column(unique=True, nullable=False)
+    password:    Mapped[str] = mapped_column(unique=True, nullable=False)
+    mail_adress: Mapped[str] = mapped_column(unique=True, nullable=False)
 
-    def __init__(self, name: str, password: str):
+    def __init__(self, name: str, password: str, mail_adress: str):
         self.name = name
         self.password = password
+        self.mail_adress = mail_adress
 
     def to_json(self):
-        return {'id': self.id, 'name': self.name, 'password': self.password}
+        return {'id': self.id, 'name': self.name, 'password': self.password, 'mail_adress': self.mail_adress}
