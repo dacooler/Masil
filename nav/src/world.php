0f7fb5ec5cdd7d32959c95c09582ac2f1a3b1b5f
Ola Eklund <ola.eklund@live.se>
2026-02-21 16:13:42 +0100

    Colision added

--- nav/src/world.php
+++ nav/src/world.php
@@ -1,11 +1,2 @@
-<div class="navThing cubed">
-  <div><iframe src="../course/ingproff.php"></iframe></div>
-  <div></div>
-  <div></div>
-  <div></div>
-  <div></div>
-  <div></div>
-  <div></div>
-</div>
 <div class="welcome"><div class="spinner"><h1>Courses and programs</h1></div></div>
 <div class="like"><button>LIKE</button></div>
